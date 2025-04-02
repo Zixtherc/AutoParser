257ef3615a2c576eadc3ef832a53c87aebfad5aa
Zixther <duckandfiretto@gmail.com>
2025-04-02 23:52:35 +0300

    fined an error, in next upd i will fix

--- modules/function_pars/parser.py
+++ modules/function_pars/parser.py
@@ -25,12 +25,11 @@ async def auto_parser(url: str, parse_mode: str, frame_dict: dict, target: str =
             # Если флаг стоит all
             if parse_mode == 'all':
                 # Парсим ВСЕ данные
-                data_html = soup.prettify()
-                print(f'Это дата html: {data_html}')
+                data_html = soup.find_all()[:200]
                 # Создаем HtmlLabel с данными
                 html_label = HtmlLabel(
                     ch_master = frame_dict["HEADER"],
-                    ch_html_text = data_html,
+                    ch_html_text = data_html[0],
                     ch_width = 150,
                     ch_height = 150,
                     ch_fg_color = '#ffffff',
@@ -38,12 +37,12 @@ async def auto_parser(url: str, parse_mode: str, frame_dict: dict, target: str =
                     ch_relief = 'groove',
                     ch_borderwidth = 3,
                 )
+                print(f'Это текст который мы размещаем: {data_html}')
                 # Размещаем
                 html_label.pack()
                 
             # В другом случае ищем конкретнные блоки
             headers = soup.find_all(target)
-            # print(f'Это хедеры{ headers}')
             # Возвращаем найденные блоки
             return headers
         # Если код не 200
